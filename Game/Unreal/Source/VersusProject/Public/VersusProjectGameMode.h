bdc166450c8f50aefa3163754cce9db6d747199a
pbausiere <pbausiere@phxlabs.ca>
2024-01-17 14:36:18 -0500

    Initial commit

---
+++ Game/Unreal/Source/VersusProject/Public/VersusProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VersusProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AVersusProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AVersusProjectGameMode();
+};
+
+
+
